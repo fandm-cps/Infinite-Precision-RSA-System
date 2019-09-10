7a2cc74b59b6e3ed04deb1c7098b0042217c3966
zxiao <zxiao@fandm.edu>
2019-09-10 16:12:02 -0400

    Test

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Zhengyi Xiao on 9/10/19.
+//  Copyright © 2019 Zhengyi Xiao. All rights reserved.
+//
+
+#include <iostream>
+class ReallyLongInt{
+    
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
