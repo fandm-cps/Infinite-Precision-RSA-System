08e4742e19472200bf4d6cb38075d36fee47780f
zxiao <zxiao@fandm.edu>
2019-10-17 16:00:21 -0400

    small fix

--- numberTheory_TEST.cpp
+++ numberTheory_TEST.cpp
@@ -16,40 +16,40 @@ TEST_CASE("TESTING numberTheory Class"){
         nT.extendedEulid(a, b, d, x, y);
 
         INFO("1 = 5*(-1)+3*(2)")
-        REQUIRE(d->toString() == "1");
-        REQUIRE(x->toString() == "-1");
-        REQUIRE(y->toString() == "2");
+        REQUIRE(d->equal(1));
+        REQUIRE(x->equal(-1));
+        REQUIRE(y->equal(2));
 
         a = ReallyLongInt(5);
         b = ReallyLongInt(5);
         nT.extendedEulid(a, b, d, x, y);
         INFO("5 = 5*(0)+5*(1)")
-        REQUIRE(d->toString() == "5");
-        REQUIRE(x->toString() == "0");
-        REQUIRE(y->toString() == "1");
+        REQUIRE(d->equal(5));
+        REQUIRE(x->equal(0));
+        REQUIRE(y->equal(1));
 
         b = ReallyLongInt(-3);
         nT.extendedEulid(a, b, d, x, y);
         INFO("-1 = 5*(1)+-3*(2)")
-        REQUIRE(d->toString() == "-1");
-        REQUIRE(x->toString() == "1");
-        REQUIRE(y->toString() == "2");
+        REQUIRE(d->equal(-1));
+        REQUIRE(x->equal(1));
+        REQUIRE(y->equal(2));
 
         a = ReallyLongInt(3);
         b = ReallyLongInt(5);
         nT.extendedEulid(a, b, d, x, y);
         INFO("1 = 3*(2)+5*(-1)")
-        REQUIRE(d->toString() == "1");
-        REQUIRE(x->toString() == "2");
-        REQUIRE(y->toString() == "-1");
+        REQUIRE(d->equal(1));
+        REQUIRE(x->equal(2));
+        REQUIRE(y->equal(-1));
 
         a = ReallyLongInt(20);
         b = ReallyLongInt(5);
         nT.extendedEulid(a, b, d, x, y);
         INFO("5 = 20*(0)+5*(1)")
-        REQUIRE(d->toString() == "5");
-        REQUIRE(x->toString() == "0");
-        REQUIRE(y->toString() == "1");
+        REQUIRE(d->equal(5));
+        REQUIRE(x->equal(0));
+        REQUIRE(y->equal(1));
         
         delete d;
         delete x;
